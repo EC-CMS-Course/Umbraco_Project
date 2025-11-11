5ca19887066a2479905a55da1e03f4d51360b4ac
Jessica Eriksson <jessicahlqvist@hotmail.com>
2025-11-11 10:26:58 +0100

    Add confirmation on QuestionFormSection and fix TempData on CallbackForm

--- Umbraco_Project/Controllers/FormController.cs
+++ Umbraco_Project/Controllers/FormController.cs
@@ -25,11 +25,11 @@ public class FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmb
         var result = _formSubmissionService.SaveCallbackRequest(model);
         if (!result)
         {
-            TempData["FormError"] = "Something went wring while subbmitting your request. Please try again.";
+            TempData["CallbackFormError"] = "Something went wring while subbmitting your request. Please try again.";
             return RedirectToCurrentUmbracoPage();
         }
 
-        TempData["FormSuccess"] = "Thank you! Your request has been recieved and we will get back to you soon";
+        TempData["CallbackFormSuccess"] = "Thank you! Your request has been recieved and we will get back to you soon";
         return RedirectToCurrentUmbracoPage();
     }
 
@@ -43,11 +43,11 @@ public class FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmb
         var result = _formSubmissionService.SaveQuestionFormRequest(model);
         if (!result)
         {
-            TempData["FormError"] = "Something went wring while subbmitting your request. Please try again.";
+            TempData["QuestionFormError"] = "Something went wring while subbmitting your question. Please try again.";
             return RedirectToCurrentUmbracoPage();
         }
 
-        TempData["FormSuccess"] = "Thank you! Your question has been recieved and we will get back to you soon";
+        TempData["QuestionFormSuccess"] = "Thank you! Your question has been recieved and we will get back to you soon";
         return RedirectToCurrentUmbracoPage();
     }
 
